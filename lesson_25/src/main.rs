549c479c3555308d4b6879b6fa1e1792fbd74721
mno <shujiang.tao@cisdi.com.cn>
2022-02-14 20:14:36 +0800

    feat(课程25): 完成学习课程25

---
+++ lesson_25/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+
+}
+// #[async_trait]
+// pub trait Fetch {
+//     type Error;
+//
+//     async fn fetch(&self) -> Result<String, Self::Error>;
+// }
+//
+// pub trait Load {
+//     type Error;
+//
+//     fn load(&self) -> Result<String, Self::Error>;
+// }
