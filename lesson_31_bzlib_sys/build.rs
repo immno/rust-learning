480121da93f175cd53ad29b87cf6fa0eea0e3723
Happy Peel <shujiang.tao@cisdi.com.cn>
2022-02-22 08:47:31 +0800

    feat(lesson31): 课程31切换环境

---
+++ lesson_31_bzlib_sys/build.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("cargo:rustc-link-lib=bz2");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file("src/bindings.rs")
+        .expect("Failed to write bindings");
+}
